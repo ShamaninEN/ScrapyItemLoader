0f04600df28e9fa6a8ef22e5a550a0877c01d30b
Shamanin <shamaninen@gmail.com>
2020-06-24 17:50:36 +0500

    first commit

---
+++ pars_lm/spiders/leroy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class LeroySpider(scrapy.Spider):
+    name = 'leroy'
+    allowed_domains = ['leroymerlin.ru']
+    start_urls = ['http://leroymerlin.ru/']
+
+    def parse(self, response):
+        pass
