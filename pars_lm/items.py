c88b114068b61db9273b528f8e0b7501f89fc95f
Shamanin <shamaninen@gmail.com>
2020-06-26 14:39:10 +0500

    Парсит и складывает в монгу.

--- pars_lm/items.py
+++ pars_lm/items.py
@@ -4,11 +4,28 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
-
+from scrapy.loader.processors import MapCompose, TakeFirst, Identity
 import scrapy
+import re
 
+def change_value(value):
+    if value:
+        value = re.sub(r"\n", '', value)
+        value_start = re.sub(r"^\s+", '', value)
+        value_end = re.sub(r"(\s\s+)", '', value_start)
+    return value_end
+def change_to_float(value):
+    if value:
+        value = float(re.sub(r"\s", '', value))
+    return value
 
 class ParsLmItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    name = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field(output_processor=Identity())
+    price = scrapy.Field(input_processor=MapCompose(change_to_float), output_processor=TakeFirst())
+    articul = scrapy.Field(output_processor=TakeFirst())
+    properties_name = scrapy.Field(input_processor=MapCompose(change_value))
+    properties_value = scrapy.Field(input_processor=MapCompose(change_value))
+    item_link = scrapy.Field(output_processor=TakeFirst())
--- pars_lm/spiders/leroy.py
+++ pars_lm/spiders/leroy.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http import HtmlResponse
+from pars_lm.items import ParsLmItem
+from scrapy.loader import ItemLoader
 
 class LeroySpider(scrapy.Spider):
     name = 'leroy'
     allowed_domains = ['leroymerlin.ru']
-    start_urls = ['http://leroymerlin.ru/']
+    def __init__(self, thing):
+        self.start_urls = [f'https://leroymerlin.ru/search/?q={thing}']
 
-    def parse(self, response):
-        pass
+
+    def parse(self, response:HtmlResponse):
+        next_page = response.xpath("//div[@class='list-paginator'] //a[contains(@class, 'next-paginator-button')]/@href").extract_first()
+        item_links = response.xpath("//div[@class='product-name']/a[contains(@class, 'product-name-inner')]/@href").extract()
+        for item_link in item_links:
+            yield response.follow(f'https://leroymerlin.ru{item_link}', callback=self.parse_item )
+
+
+        yield response.follow(next_page, callback=self.parse)
+
+    def parse_item(self, response:HtmlResponse):
+        loader = ItemLoader(item=ParsLmItem(), response=response)
+        loader.add_xpath('photos', "//source[contains(@media, '1024px')]/@data-origin")
+        loader.add_xpath('name', "//h1[@class='header-2']/text()")
+        loader.add_xpath('articul', "//span[@slot='article']/@content")
+        loader.add_xpath('price', "//span[@slot='price']/text()")
+        loader.add_xpath('properties_name', "//div[@class='def-list__group']/dt/text()")
+        loader.add_xpath('properties_value', "//div[@class='def-list__group']/dd/text()")
+        loader.add_value('item_link', response.url)
+        yield loader.load_item()
